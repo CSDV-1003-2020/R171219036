1b6298461dc04cae56eb1fae02ab5ad3fa733eb6
SnigdhaMarkanday <snigdhamarkanday@gmail.com>
2020-03-27 16:20:19 +1300

    added a comment

--- complex_number.cpp
+++ complex_number.cpp
@@ -12,7 +12,8 @@ int main()
 	
 		 cout<< "enter real and imaginary parts:"<<endl; //getting the values
 		 cin>> n1.r>>n1.i>> n2.r>>n2.i; 
-		 sum.r=n1.r+n2.r;  // addition of real part 
+
+		sum.r=n1.r+n2.r;  // addition of real part 
 		 sum.i=n1.i+n2.r;  // addition of imaginary part
 	
 	// displaying the sum of complex number
@@ -23,4 +24,5 @@ int main()
 		 return 0;
 		 }
 
+//sum of 2 complex no.s:wq
 
