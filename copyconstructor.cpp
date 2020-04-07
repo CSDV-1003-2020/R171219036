8508957b17934aab59b10fa5b3873fb9bfc50a56
sunnyraj5555 <headheal2206@gmail.com>
2020-04-07 11:10:30 +0530

    main function

--- copyconstructor.cpp
+++ copyconstructor.cpp
@@ -16,3 +16,15 @@ public:
     x = px, y = py;
   }
 };
+
+int main(void) 
+{
+  // Define an array of size 10 & of type point
+  // This line will cause error
+
+
+  point a[10];
+
+  // Remove above line and program will compile without error
+  point b = point(5, 6);
+}
