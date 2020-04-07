bbca7a4819df0dc27ed7ae010d0977c3194fdd54
sunnyraj5555 <headheal2206@gmail.com>
2020-04-07 11:16:43 +0530

    constructor with class created

---
+++ destrctor.cpp
@@ -0,0 +1,13 @@
+//Destructor is a member function which destructs or deletes an object.
+//Destructors have same name as the class preceded by a tilde (~)
+//Destructors don’t take any argument and don’t return anything
+
+
+#include <iostream>
+using namespace std;
+class HelloWorld{
+public:
+  //Constructor
+  HelloWorld(){
+    cout<<"Constructor is called"<<endl;
+  }
