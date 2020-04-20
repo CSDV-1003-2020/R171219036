2aafcc8de8f746944749b6575a15c805b57366b7
sunnyraj5555 <headheal2206@gmail.com>
2020-04-20 14:28:15 +0530

    storing and displaying information

--- structure.cpp
+++ structure.cpp
@@ -7,3 +7,34 @@ struct student
     int roll;
     float marks;
 } s[10];
+
+int main()
+{
+    cout << "Enter information of students: " << endl;
+
+    for(int i = 0; i < 10; ++i)
+    {
+        s[i].roll = i+1;
+        cout << "For roll number" << s[i].roll << "," << endl;
+
+        cout << "Enter name: ";
+        cin >> s[i].name;
+
+        cout << "Enter marks: ";
+        cin >> s[i].marks;
+
+        cout << endl;
+    }
+
+    cout << "Displaying Information: " << endl;
+
+
+    for(int i = 0; i < 10; ++i)
+    {
+        cout << "\nRoll number: " << i+1 << endl;
+        cout << "Name: " << s[i].name << endl;
+        cout << "Marks: " << s[i].marks << endl;
+    }
+
+    return 0;
+}
